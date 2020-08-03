3f3c8dc871ebe09f07ed5614aa26ae9d426ad21d
Antonio David Villegas <advy99@gmail.com>
2020-08-03 14:40:02 +0200

    comenzamos a crear clases para objetos

---
+++ include/GameObject.h
@@ -0,0 +1,36 @@
+#ifndef GAMEOBJECT_H_INCLUDED
+#define GAMEOBJECT_H_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "TextureManager.h"
+#include <string>
+
+class GameObject {
+	private:
+
+	protected:
+		int x_pos;
+		int y_pos;
+
+		std::string texture_id;
+
+		int current_frame;
+		int current_row;
+
+		int width;
+		int height;
+
+	public:
+		void load(const int X, const int Y, const int WIDTH, const int HEIGHT,
+					 const std::string id);
+		void draw(SDL_Renderer * g_renderer);
+		void update();
+		void clean();
+
+};
+
+
+
+
+#endif
