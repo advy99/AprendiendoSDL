2d2b6b9d7d4be9f08501cfdd559349e4045820b8
Antonio David Villegas <advy99@gmail.com>
2020-08-09 15:41:36 +0200

    comenzamos con clase para grafico animado

---
+++ src/AnimatedGraphic.cpp
@@ -0,0 +1,12 @@
+#include "AnimatedGraphic.h"
+#include "TextureManager.h"
+
+AnimatedGraphic::AnimatedGraphic(const LoaderParams * params, const int speed)
+											:SDLGameObject(params), animation_speed(speed){
+
+}
+
+void AnimatedGraphic::update() {
+	current_frame = int( (SDL_GetTicks() / ( 1000 / animation_speed ))
+								% TextureManager::getTextureNumFrames(texture_id);
+}
