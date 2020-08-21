f4e2ab8c1576aed19eb950ebbf4560fae856dc76
Antonio David Villegas <advy99@gmail.com>
2020-08-21 17:57:20 +0200

    open SDL audio and ensure we clean all the sounds

--- src/SoundManager.cpp
+++ src/SoundManager.cpp
@@ -1,5 +1,21 @@
 #include "SoundManager.h"
 
+SoundManager::SoundManager() {
+	Mix_OpenAudio(22050, AUDIO_S16, 2, 4096);
+}
+
+SoundManager::~SoundManager() {
+
+	for ( auto it = sound_fxs.begin(); it != sound_fxs.end(); ++it ) {
+		Mix_FreeChunk( (*it).second );
+	}
+
+	for ( auto it = music.begin(); it != music.end(); ++it ) {
+		Mix_FreeMusic( (*it).second );
+	}
+
+}
+
 
 SoundManager * SoundManager::getInstance() {
 	if ( instance == nullptr ) {
